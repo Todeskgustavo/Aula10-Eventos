ea1fc2b3e7f4e5f52cf7d58be2927b0e97cbb0de
Gustavo Todeschini <gutodeschinii@outlook.com>
2024-04-17 22:52:25 -0300

    testes com eventos

--- javascript.js
+++ javascript.js
@@ -1,5 +1,32 @@
+/* teste de onclick
 document.getElementById('meuBotao').onclick = function() {
     this.innerHTML = 'Texto alterado';
     this.style.backgroundColor = 'blue';
     this.style.color = 'white';
 };
+*/
+
+function eventoDblClick(){
+    alert('Evento de clique duplo');
+}
+
+function viraVermelho(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "red";
+}
+
+function viraAzul(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "blue";
+}
+
+/* -- Função demonstrando o evento onmousemove 
+function adicionaTexto(){
+    let p = document.getElementById("texto");
+    p.append('o mouse moveu<br>');
+}
+*/
+
+function mudou(){
+    console.log('Mudou');
+}
